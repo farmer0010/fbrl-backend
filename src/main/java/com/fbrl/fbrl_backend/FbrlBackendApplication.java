d5fc2e8cb1d6d526b0291bac6d3796cfcb04ae26
farmer0010 <zmqkf16@naver.com>
2026-07-24 15:19:53 +0900

    chore: Spring Boot 4.0.7 기반 초기 프로젝트 설정

---
+++ src/main/java/com/fbrl/fbrl_backend/FbrlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fbrl.fbrl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbrlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbrlBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fbrl/fbrl_backend/FbrlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbrl.fbrl_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbrlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
